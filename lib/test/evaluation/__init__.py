171cb0a2f14cf76bbd7ccbeb016541eda5f91c6f
Tong Shen <tongshen@v004.ib.bridges2.psc.edu>
2023-07-10 00:05:28 -0400

    ngc

--- lib/test/evaluation/__init__.py
+++ lib/test/evaluation/__init__.py
@@ -1,4 +1,4 @@
 from .data import Sequence
 from .tracker import Tracker, trackerlist
 from .datasets import get_dataset
-from .environment import create_default_local_file_ITP_test
+from .environment import create_default_local_file_ITP_test, create_default_local_file_ITP_test_ngc
--- lib/train/admin/environment.py
+++ lib/train/admin/environment.py
@@ -89,6 +89,52 @@ def create_default_local_file_ITP_train(workspace_dir, data_dir):
             else:
                 f.write('        self.{} = \'{}\'    # {}\n'.format(attr, attr_val, comment_str))
 
+def create_default_local_file_ITP_train_ngc(workspace_dir, data_dir):
+    path = os.path.join(os.path.dirname(__file__), 'local.py')
+
+    empty_str = '\'\''
+    default_settings = OrderedDict({
+        'workspace_dir': workspace_dir,
+        'tensorboard_dir': os.path.join(workspace_dir, 'tensorboard'),    # Directory for tensorboard files.
+        'pretrained_networks': os.path.join(workspace_dir, 'pretrained_networks'),
+        'lasot_dir': os.path.join(data_dir, 'lasot_new/LaSOTBenchmark'),
+        'got10k_dir': os.path.join(data_dir, 'got10k', 'train'),
+        'lasot_lmdb_dir': os.path.join(data_dir, 'lasot_lmdb'),
+        'got10k_lmdb_dir': os.path.join(data_dir, 'got10k_lmdb'),
+        'trackingnet_dir': os.path.join(data_dir, 'trackingnet'),
+        'trackingnet_lmdb_dir': os.path.join(data_dir, 'trackingnet_lmdb'),
+        'coco_dir': os.path.join(data_dir, 'coco'),
+        'coco_lmdb_dir': os.path.join(data_dir, 'coco_lmdb'),
+        'tnl2k_dir': os.path.join(data_dir, 'tnl2k'),
+        'lvis_dir': empty_str,
+        'sbd_dir': empty_str,
+        'imagenet_dir': os.path.join(data_dir, 'vid'),
+        'imagenet_lmdb_dir': os.path.join(data_dir, 'vid_lmdb'),
+        'imagenetdet_dir': empty_str,
+        'ecssd_dir': empty_str,
+        'hkuis_dir': empty_str,
+        'msra10k_dir': empty_str,
+        'davis_dir': empty_str,
+        'youtubevos_dir': empty_str})
+
+    comment = {'workspace_dir': 'Base directory for saving network checkpoints.',
+               'tensorboard_dir': 'Directory for tensorboard files.'}
+
+    with open(path, 'w') as f:
+        f.write('class EnvironmentSettings:\n')
+        f.write('    def __init__(self):\n')
+
+        for attr, attr_val in default_settings.items():
+            comment_str = None
+            if attr in comment:
+                comment_str = comment[attr]
+            if comment_str is None:
+                if attr_val == empty_str:
+                    f.write('        self.{} = {}\n'.format(attr, attr_val))
+                else:
+                    f.write('        self.{} = \'{}\'\n'.format(attr, attr_val))
+            else:
+                f.write('        self.{} = \'{}\'    # {}\n'.format(attr, attr_val, comment_str))
 
 def env_settings():
     env_module_name = 'lib.train.admin.local'
